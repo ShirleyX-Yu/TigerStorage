78a98f023adb45ceef70818497c9edc94c1e08a7
Cindy <cindytsacc@gmail.com>
2025-03-25 02:49:27 -0400

    testing read/write

--- backend/test_insert.py
+++ backend/test_insert.py
@@ -1,26 +1,33 @@
 from database import get_db_connection
 
-def test_insert():
+# simple program to insert dummy data and select all storage_listing data in database
 
+# writing to database
+def test_insert():
     with get_db_connection() as conn:
         with conn.cursor() as cur:
             cur.execute(
-                "INSERT INTO sellers (seller_id, name, email) VALUES (1, 'John Doe', 'john@example.com')"
-
+                "INSERT INTO storage_listings (listing_id, location, cost, cubic_ft, contract_length) "
+                "VALUES (%s, %s, %s, %s, %s) RETURNING listing_id",
+                (1, "123 Test Str", 100, 500, "3 months")
             )
 
-
-            cur.execute(
-                "INSERT INTO storage_listings (seller_id, title, description, location, square_ft, amount, available_from, available_to) "
-                "VALUES (%s, %s, %s, %s, %s, %s, %s, %s) RETURNING listing_id",
-                (1, "Test Storage", "A test listing", "123 Test St", 500, 1000, "2024-04-01", "2024-12-01")
-            )
-
-            listing_id = cur.fetchone()[0]
-            conn.commit()
+            listing_id = cur.fetchone()[0] # correctly inserts storage listing with id 1
+            conn.commit() # saves changes permanently in database
 
             print(f"Inserted test listing with ID: {listing_id}")
 
+# reading to database
+def test_select():
+    with get_db_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                "SELECT * FROM storage_listings"
+            )
+
+            table = cur.fetchall()
+            print(table) # view all data in storage_listings
 
 if __name__ == "__main__":
     test_insert()
+    test_select()
