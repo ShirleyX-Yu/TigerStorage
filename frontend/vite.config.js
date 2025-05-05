ebae69725bc44c0d4da8ef73b3438f46a3602835
Diya Hundiwala <dh3163@princeton.edu>
2025-05-05 19:28:13 -0400

    got rid of legacy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
-import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), legacy({ targets: ['defaults', 'not IE 11'] })],
+  plugins: [react()],
   server: {
     port: 5173,
     proxy: {
