c5c36cac4d986f2422448da4cfa3b22992e862e9
Cindy <cindytsacc@gmail.com>
2025-05-07 18:35:30 -0400

    add undergrad admin access

--- backend/app.py
+++ backend/app.py
@@ -95,7 +95,7 @@ def catch_all(path=''):
     if 'CAS_USERNAME' in session:
         username = session.get('CAS_USERNAME')
         # Set user type based on username
-        if username in ['cs-tigerstorage', 'jiaweim', 'cs-rdondero']:
+        if username in ['cs-tigerstorage', 'jiaweim', 'cs-rdondero', 'jd9751']:
             session['user_type'] = 'admin'
             # If accessing root, redirect admin to admin dashboard
             if not path:
